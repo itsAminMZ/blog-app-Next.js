d8fed7242d1ec526cbf002717a50c2385d4d980c
Amin <aminmohammadzadeh101@gmail.com>
2024-07-13 17:59:12 +0330

    second change

--- src/app/api/blog/route.tsx
+++ src/app/api/blog/route.tsx
@@ -1,37 +1,40 @@
 import { NextRequest } from "next/server";
 
-let blogs:any = []
-let blogIndex = blogs.length
-
+let blogs:any = [];
+let blogIndex = 0;
 
 export async function POST(req: Request, res: Response) {
   const data = await req.json();
   const header = data.header;
   const author = data.author;
   const text = data.text;
-  const id = blogIndex
-//   blogs.push({header,author,text,id})
-  blogs[blogIndex]= {header,author,text,id}
-blogIndex++
+  const id = blogIndex;
+  blogs[blogIndex] = { header, author, text, id };
+  blogIndex++;
 
-  return Response.json({ operation: "done"},{status:200});
+  return new Response(JSON.stringify({ operation: "done" }), { status: 200 });
 }
 
 export async function GET(req: Request, res: Response) {
-//   const users = await Users.findAll();
-  return Response.json({data: blogs});
+  return new Response(JSON.stringify({ data: blogs }));
 }
 
 export async function DELETE(req: NextRequest, res: Response) {
   const id = req.nextUrl.searchParams.get("id");
-  if(!id){
-    return
+  if (id === null || isNaN(Number(id))) {
+    return new Response(JSON.stringify({ error: "Invalid ID" }), { status: 400 });
   }
-    console.log('id in database',id);
-    blogs[id].remove()
+
+  const index = Number(id);
+  if (index < 0 || index >= blogs.length) {
+    return new Response(JSON.stringify({ error: "ID out of bounds" }), { status: 404 });
+  }
+
+  blogs.splice(index, 1);
   
-//   blogs[id]
-  
-//   Users.destroy({ where: { id } });
-  return Response.json({ operation: "done" });
+  // برای بروزرسانی مقادیر id بعد از حذف یک عنصر
+  blogs = blogs.map((blog:any, idx:any) => ({ ...blog, id: idx }));
+  blogIndex = blogs.length;
+
+  return new Response(JSON.stringify({ operation: "done" }), { status: 200 });
 }
